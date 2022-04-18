be4a76e5020770b1f05a181773d0f352d8431b34
Maxim Tarend <84102129+MaximTarend@users.noreply.github.com>
2022-04-18 16:53:59 +0300

    + Loading

--- app/src/main/java/by/hometrainng/listapi/adapter/ItemAdapter.kt
+++ app/src/main/java/by/hometrainng/listapi/adapter/ItemAdapter.kt
@@ -8,31 +8,54 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import by.hometrainng.listapi.databinding.ItemBinding
-import by.hometrainng.listapi.model.Character
+import by.hometrainng.listapi.databinding.LoadingItemBinding
+import by.hometrainng.listapi.model.ListElement
+
 
 class ItemAdapter(
     context: Context
-): ListAdapter<Character, CharacterViewHolder>(DIFF_UTIL) {
+): ListAdapter<ListElement, RecyclerView.ViewHolder>(DIFF_UTIL) {
 
     private val layoutInflater = LayoutInflater.from(context)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
-        return CharacterViewHolder(
-            binding = ItemBinding.inflate(layoutInflater, parent, false)
-        )
+    override fun getItemViewType(position: Int): Int {
+        return when(getItem(position)) {
+            is ListElement.Character -> TYPE_CHARACTER
+            ListElement.Loading -> TYPE_LOADING
+        }
     }
 
-    override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
-        holder.bind(getItem(position))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+                    TYPE_CHARACTER -> {
+                        CharacterViewHolder(binding = ItemBinding.inflate(layoutInflater, parent, false))
+                    }
+                    TYPE_LOADING -> {
+                        LoadingViewHolder(binding = LoadingItemBinding.inflate(layoutInflater, parent, false))
+                    }
+            else -> {
+                error("Unknown View Type")}
+        }
     }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder !is CharacterViewHolder) return
+        val item = getItem(position) as? ListElement.Character ?: return
+        holder.bind(item)
+    }
+
 
     companion object {
-        private val  DIFF_UTIL = object : DiffUtil.ItemCallback<Character>() {
-            override fun areItemsTheSame(oldItem: Character, newItem: Character): Boolean {
+
+        private const val TYPE_CHARACTER = 0
+        private const val TYPE_LOADING = 1
+
+        private val  DIFF_UTIL = object : DiffUtil.ItemCallback<ListElement>() {
+            override fun areItemsTheSame(oldItem: ListElement, newItem: ListElement): Boolean {
                 return oldItem == newItem
             }
 
-            override fun areContentsTheSame(oldItem: Character, newItem: Character): Boolean {
+            override fun areContentsTheSame(oldItem: ListElement, newItem: ListElement): Boolean {
                 return oldItem == newItem
             }
         }
@@ -43,9 +66,12 @@ class CharacterViewHolder(
     private val binding: ItemBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind (item: Character) {
+    fun bind (item: ListElement.Character) {
         binding.itemText.text = item.login
     }
-
 }
+
+class LoadingViewHolder(
+    binding: LoadingItemBinding
+) : RecyclerView.ViewHolder(binding.root)
 
